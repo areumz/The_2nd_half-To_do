5f997694ff6d97649f5c571561bd650c2de687bd
Kim A Reum <ksah1016@gmail.com>
2021-10-31 17:07:09 +0900

    quotes modified second

--- js/quotes.js
+++ js/quotes.js
@@ -1,42 +1,42 @@
 const quotes = [
     {
         quote: "When you go through hardships and decide not to surrender, that is strength",
-        author: "Arnold Schwarzenegger",
+        author: " -Arnold Schwarzenegger",
     },
     {
         quote: "It is kind of fun to do the impossible",
-        author: "Walt Disney",
+        author: " -Walt Disney",
     },
     {   quote: "There are better starters than me but I’m a strong finisher",
-        author: "Usain Bolt",
+        author: " -Usain Bolt",
     },
     {
         quote: "Tough times never last, but tough people do",
-        author: "Robert H. Schuller",
+        author: " -Robert H. Schuller",
     },
     {
         quote: "I’ve failed over and over and over again in my life and that is why I succeed",
-        author: "Michael Jordan",
+        author: " -Michael Jordan",
     },
     {
         quote: "If you don’t get out of the box you’ve been raised in, you won’t understand how much bigger the world is",
-        author: "Angelina Jolie",
+        author: " -Angelina Jolie",
     },
     {
         quote: "But I know, somehow, that only when it is dark enough can you see the stars",
-        author: "Martin Luther King, Jr",
+        author: " -Martin Luther King, Jr",
     },
     {
         quote: "Grind Hard, Shine Hard",
-        author: "Dwayne Johnson",
+        author: " -Dwayne Johnson",
     },
     {
         quote: "I didn’t get there by wishing for it or hoping for it, but by working for it",
-        author: "Estée Lauder",
+        author: " -Estée Lauder",
     },
     {
         quote: "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time",
-        author: "Thomas Edison",
+        author: " -Thomas Edison",
     }
 ];
 
@@ -47,3 +47,6 @@ const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todaysQuote.quote;
 author.innerText = todaysQuote.author;
+
+author.classList.add("author", "quoteColor");
+quote.classList.add("quoteColor");
