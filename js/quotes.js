92fe869ac9504b5014beb01ab5d3b4a8faaa0a14
Kim A Reum <ksah1016@gmail.com>
2021-10-31 16:38:35 +0900

    quotes modified

--- js/quotes.js
+++ js/quotes.js
@@ -40,8 +40,8 @@ const quotes = [
     }
 ];
 
-const quote = document.getElementById("quote span:first-child");
-const author = document.getElementById("quote span:last-child");
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
 
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
